b243c81481e017aa004ec7378b706675c1ec7637
nuttyblood <nutty.blood@gmail.com>
2025-09-18 11:20:14 +0200

    control de versiones

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+version="1"
+
 pdflatex -shell-scape main.tex
 pdflatex -shell-scape main.tex
 bibtex main.aux
 bibtex main.aux
 pdflatex -shell-scape main.tex
 pdflatex -shell-scape main.tex
+
+cp main.tex versions/ECRH_modulation_v${version}.tex
+cp main.pdf versions/ECRH_modulation_v${version}.pdf
