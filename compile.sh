577c53af5ff5c202239a155e9cf82d8d20a34140
milligen <boudewijn.vanmilligen@gmail.com>
2025-09-22 11:53:52 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Si desea actualizar la actual version del script, por favor aumente una unidad el valor entre comillas
-version = "1" 
+version="1" 
+echo "Version = "$version
 
 # compilacion del documento
 pdflatex -shell-scape main.tex
@@ -12,5 +13,5 @@ pdflatex -shell-scape main.tex
 pdflatex -shell-scape main.tex
 
 # guardado de versiones
-cp main.tex versions/ECRH_modulation_v${version}.tex
-cp main.pdf versions/ECRH_modulation_v${version}.pdf
+cp main.tex versions/ECRH_modulation_v$version.tex
+cp main.pdf versions/ECRH_modulation_v$version.pdf
